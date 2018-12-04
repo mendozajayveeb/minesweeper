cd90ccce4c879f5003b6db5a20d1d5943fd80a24
Jayvee Mendoza <mendozajayveeb@gmail.com>
2018-12-04 16:04:30 +0800

    includes minesweeper game

---
+++ assets/js/script.js
@@ -0,0 +1,213 @@
+$(function(){
+
+	$('.remove1').on('click', function(){
+		$(this).css('visibility', 'hidden');
+		$('.remove2').css('visibility', 'hidden');
+		$('.remove3').css('visibility', 'hidden');
+	});
+
+	$('.remove2').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove3').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove4').on('click', function(){
+		$(this).css('visibility', 'hidden');
+		
+		setTimeout(function() { 
+			$('.lost').slideToggle();
+		}, 600);
+		
+	});
+
+	$('.remove5').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove6').on('click', function(){
+		$(this).css('visibility', 'hidden');
+		$('.remove12').css('visibility', 'hidden');
+		$('.remove18').css('visibility', 'hidden');
+		$('.remove24').css('visibility', 'hidden');
+	});
+
+	$('.remove7').on('click', function(){
+		$(this).css('visibility', 'hidden');
+		
+		setTimeout(function() { 
+			$('.lost').slideToggle();
+		}, 600);
+
+	});
+
+	$('.remove8').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove9').on('click', function(){
+		$(this).css('visibility', 'hidden');
+		
+		setTimeout(function() { 
+			$('.lost').slideToggle();
+		}, 600);
+
+	});
+
+	$('.remove10').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove11').on('click', function(){
+		$(this).css('visibility', 'hidden');
+		
+		setTimeout(function() { 
+			$('.lost').slideToggle();
+		}, 600);
+
+	});
+
+	$('.remove12').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove13').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove14').on('click', function(){
+		$(this).css('visibility', 'hidden');
+		
+		setTimeout(function() { 
+			$('.lost').slideToggle();
+		}, 600);
+
+	});
+
+	$('.remove15').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove16').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove17').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove18').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove19').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove20').on('click', function(){
+		$(this).css('visibility', 'hidden');
+		
+		setTimeout(function() { 
+			$('.lost').slideToggle();
+		}, 600);
+
+	});
+
+	$('.remove21').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove22').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove23').on('click', function(){
+		$(this).css('visibility', 'hidden');
+		
+		setTimeout(function() { 
+			$('.lost').slideToggle();
+		}, 600);
+
+	});
+
+	$('.remove24').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove25').on('click', function(){
+		$(this).css('visibility', 'hidden');
+		$('.remove26').css('visibility', 'hidden');
+		$('.remove32').css('visibility', 'hidden');
+	});
+
+	$('.remove26').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove27').on('click', function(){
+		$(this).css('visibility', 'hidden');
+		
+		setTimeout(function() { 
+			$('.lost').slideToggle();
+		}, 600);
+
+	});
+
+	$('.remove28').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove29').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove30').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove31').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove32').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove33').on('click', function(){
+		$(this).css('visibility', 'hidden');
+		
+		setTimeout(function() { 
+			$('.lost').slideToggle();
+		}, 600);
+
+	});
+
+	$('.remove34').on('click', function(){
+		$(this).css('visibility', 'hidden');
+		
+		setTimeout(function() { 
+			$('.lost').slideToggle();
+		}, 600);
+
+	});
+
+	$('.remove35').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	});
+
+	$('.remove36').on('click', function(){
+		$(this).css('visibility', 'hidden');
+	
+		setTimeout(function() { 
+			$('.lost').slideToggle();
+		}, 600);
+
+	});
+
+	$('#restart').on('click', function(){
+		location.reload();
+	});
+
+});
